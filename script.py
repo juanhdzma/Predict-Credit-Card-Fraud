47ba5cdc125b52544ac269aa7e6034e746bc0c2f
Juanhdzma <juanhema@unisabana.edu.co>
2022-10-27 18:34:51 -0500

    Model build

--- script.py
+++ script.py
@@ -1,44 +1,46 @@
 import seaborn
 import pandas as pd
 import numpy as np
-import codecademylib3
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import codecademylib3
 
 # Load the data
 transactions = pd.read_csv('transactions_modified.csv')
 print(transactions.head())
-print(transactions.info())
+#print(transactions.info())
 
 # How many fraudulent transactions?
-
+print(sum(transactions.isFraud == 1))
 
 # Summary statistics on amount column
-
+transactions["amount"].describe()
 
 # Create isPayment field
-
+transactions["isPayment"] = 1 if transactions["type"] is ("PAYMENT" or "DEBIT") else 0
 
 # Create isMovement field
-
+transactions["isMovement"] = 1 if transactions["type"] is ("CASH_OUT" or "TRANSFER") else 0
 
 # Create accountDiff field
-
+transactions["accountDiff"] = abs(transactions["oldbalanceOrg"] - transactions["oldbalanceDest"] )
 
 # Create features and label variables
-
+features = [transactions["amount"], transactions["isPayment"], transactions["isMovement"], transactions["accountDiff"]]
+label = transactions["isFraud"]
 
 # Split dataset
-
+x_train, x_test, y_train, y_test = train_test_split(features, label, train_size=0.7, test_size=0.3)
 
 # Normalize the features variables
-
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
 
 # Fit the model to the training data
-
+model = LogisticRegression()
+model.fit(x_train, y_train)
 
 # Score the model on the training data
 
