3d723043f3814118d983105bef59a0adbc8a4aa4
Juanhdzma <juanhema@unisabana.edu.co>
2022-10-27 18:44:31 -0500

    Final commit

--- script.py
+++ script.py
@@ -18,20 +18,26 @@ print(sum(transactions.isFraud == 1))
 transactions["amount"].describe()
 
 # Create isPayment field
-transactions["isPayment"] = 1 if transactions["type"] is ("PAYMENT" or "DEBIT") else 0
+transactions["isPayment"] = 1 if transactions["type"] is ("PAYMENT"
+                                                          or "DEBIT") else 0
 
 # Create isMovement field
-transactions["isMovement"] = 1 if transactions["type"] is ("CASH_OUT" or "TRANSFER") else 0
+transactions["isMovement"] = 1 if transactions["type"] is (
+    "CASH_OUT" or "TRANSFER") else 0
 
 # Create accountDiff field
-transactions["accountDiff"] = abs(transactions["oldbalanceOrg"] - transactions["oldbalanceDest"] )
+transactions["accountDiff"] = abs(transactions["oldbalanceOrg"] -
+                                  transactions["oldbalanceDest"])
 
 # Create features and label variables
-features = [transactions["amount"], transactions["isPayment"], transactions["isMovement"], transactions["accountDiff"]]
+features = transactions[["amount", "isPayment", "isMovement", "accountDiff"]]
 label = transactions["isFraud"]
 
 # Split dataset
-x_train, x_test, y_train, y_test = train_test_split(features, label, train_size=0.7, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(features,
+                                                    label,
+                                                    train_size=0.7,
+                                                    test_size=0.3)
 
 # Normalize the features variables
 scaler = StandardScaler()
@@ -43,13 +49,14 @@ model = LogisticRegression()
 model.fit(x_train, y_train)
 
 # Score the model on the training data
-
+print(model.score(x_train, y_train))
 
 # Score the model on the test data
-
+print(model.score(x_test, y_test))
 
 # Print the model coefficients
-
+print(model.coef_)
+print(model.intercept_)
 
 # New transaction data
 transaction1 = np.array([123456.78, 0.0, 1.0, 54670.1])
@@ -57,15 +64,17 @@ transaction2 = np.array([98765.43, 1.0, 0.0, 8524.75])
 transaction3 = np.array([543678.31, 1.0, 0.0, 510025.5])
 
 # Create a new transaction
-
+your_transaction = np.array([57.3, 0.0, 1.0, 125.3])
 
 # Combine new transactions into a single array
-
+sample_transactions = np.stack(
+    (transaction1, transaction2, transaction3, your_transaction))
 
 # Normalize the new transactions
-
+sample_transactions = scaler.fit_transform(sample_transactions)
 
 # Predict fraud on the new transactions
-
+print(model.predict(sample_transactions))
 
 # Show probabilities on the new transactions
+print(model.predict_proba(sample_transactions))
